b80617ea98b4518f4d1b378811cfeb445e5982e9
Joao Victor <joaovictorcostabarbosac@gmail.com>
2025-11-07 01:57:15 -0300

    Main atualizada para incluir rotas de autenticação

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.routers import auth_routers
 
 app = FastAPI(title="Marketplace Agro", version="0.1.0")
+
+app.include_router(auth_routers.router, prefix="/api/v1/auth", tags=["auth"])
 
 @app.get("/")
 async def root():
