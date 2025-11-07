fc397996a661cbe06a124f9c7d55e6ac121431a2
Joao Victor <joaovictorcostabarbosac@gmail.com>
2025-11-07 01:54:50 -0300

    Criado as rotas de autenticação do user

---
+++ app/routers/auth_routers.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, status, Depends
+from app.schemas.user_schema import UserCreate, UserLogin, UserResponse, UserTokenResponse
+from app.services.auth_service import AuthService
+from app.core.dependencies import get_auth_service
+
+router = APIRouter()
+
+@router.post("/register", response_model=UserTokenResponse)
+async def register(
+    user_data: UserCreate,
+    auth_service: AuthService = Depends(get_auth_service)
+):
+    return await auth_service.create_user(user_data)
+
+@router.post("/login", response_model=UserTokenResponse)
+async def login(
+    user_data: UserLogin,
+    auth_service: AuthService = Depends(get_auth_service)
+):
+    return await auth_service.login(user_data)
